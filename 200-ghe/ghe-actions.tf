99dd3dd42650fab002ffefab27bd2497097c4c14
nikAstanin <52233130+nikAstanin@users.noreply.github.com>
2024-03-28 09:16:33 +0200

    Update ghe-actions.tf

--- 200-ghe/ghe-actions.tf
+++ 200-ghe/ghe-actions.tf
@@ -2,3 +2,10 @@ data "aws_iam_user" "ghe-actions-user" {
   user_name = "test-ghe-actions-user"
 }
 
+resource "aws_iam_access_key" "gh_actions_user_key" {
+  user = aws_iam_user.ghe-actions-user.name
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
