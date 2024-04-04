5dabda19d5e84307ed345030564630dcdfc87b66
nikAstanin <52233130+nikAstanin@users.noreply.github.com>
2024-04-04 17:59:45 +0300

    Fixed name in ghe-actions.tf

--- 200-ghe/ghe-actions.tf
+++ 200-ghe/ghe-actions.tf
@@ -3,7 +3,7 @@ resource "aws_iam_user" "ghe_actions_user" {
 }
 
 resource "aws_iam_access_key" "gh_actions_user_key" {
-  user = aws_iam_user.ghe-actions-user.name
+  user = aws_iam_user.ghe_actions_user.name
 
   lifecycle {
     create_before_destroy = true
