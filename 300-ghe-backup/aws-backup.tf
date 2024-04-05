58687ac76d3c4172496016b98520099b65f32f0b
nikAstanin <52233130+nikAstanin@users.noreply.github.com>
2024-04-05 11:01:20 +0300

    Update aws-backup.tf

--- 300-ghe-backup/aws-backup.tf
+++ 300-ghe-backup/aws-backup.tf
@@ -1,1 +1,11 @@
+resource "aws_iam_user" "ghe_backup_user" {
+  name = "${var.branch}-ghe-backup-user"
+}
 
+resource "aws_iam_access_key" "gh_backup_user_key" {
+  user = aws_iam_user.ghe_backup_user.name
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
