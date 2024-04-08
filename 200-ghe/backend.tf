e82cb25c074a2f831f6805f4878026767742f614
nikAstanin <52233130+nikAstanin@users.noreply.github.com>
2024-04-08 15:42:58 +0300

    Update backend.tf

--- 200-ghe/backend.tf
+++ 200-ghe/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "test-key-rotation-state-bucket-gh"
-    key    = "state/"
+    key    = "state/200-ghe/terraform.tfstate"
     region = "us-east-1"
     # dynamodb_table = "my-lock-table"
   }
