0dcdba51571198b3404cde090a3acde9af749333
nikAstanin <52233130+nikAstanin@users.noreply.github.com>
2024-04-24 12:15:04 +0300

    Small fix in 200-ghe/ghe-actions.tf

--- 200-ghe/ghe-actions.tf
+++ 200-ghe/ghe-actions.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_user" "ghe_actions_user" {
-  for_each = toset(var.branches)  # where var.branches is a list of branches ['dev', 'stage', 'prod']
+  for_each = toset(var.branch)  # where var.branch is a list of branches ['dev', 'stage', 'prod']
 
   name = "${each.key}-ghe-actions-user"
 
@@ -9,7 +9,7 @@ resource "aws_iam_user" "ghe_actions_user" {
 }
 
 resource "aws_iam_access_key" "gh_actions_user_key" {
-  for_each = toset(var.branches)
+  for_each = toset(var.branch)
 
   user = aws_iam_user.ghe_actions_user[each.key].name
 
