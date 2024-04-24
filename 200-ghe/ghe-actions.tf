8caa06c53ca4ba958098f187af780bbfdd7aafe5
nikAstanin <52233130+nikAstanin@users.noreply.github.com>
2024-04-24 12:08:06 +0300

    Added for_each to ghe-actions.tf in 200-ghe

--- 200-ghe/ghe-actions.tf
+++ 200-ghe/ghe-actions.tf
@@ -1,9 +1,17 @@
 resource "aws_iam_user" "ghe_actions_user" {
-  name = "${var.branch}-ghe-actions-user"
+  for_each = toset(var.branches)  # where var.branches is a list of branches ['dev', 'stage', 'prod']
+
+  name = "${each.key}-ghe-actions-user"
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 resource "aws_iam_access_key" "gh_actions_user_key" {
-  user = aws_iam_user.ghe_actions_user.name
+  for_each = toset(var.branches)
+
+  user = aws_iam_user.ghe_actions_user[each.key].name
 
   lifecycle {
     create_before_destroy = true
