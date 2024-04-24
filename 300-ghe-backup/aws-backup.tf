37b54335466a7b613f850ef36c4fa5dfe2d562a5
nikAstanin <52233130+nikAstanin@users.noreply.github.com>
2024-04-24 12:49:02 +0300

    Update aws-backup.tf in 300-ghe-backup

--- 300-ghe-backup/aws-backup.tf
+++ 300-ghe-backup/aws-backup.tf
@@ -1,9 +1,17 @@
 resource "aws_iam_user" "ghe_backup_user" {
-  name = "${var.branch}-ghe-backup-user"
+  for_each = toset(var.branch)  # where var.branch is a list of branches ['dev', 'stage', 'prod']
+
+  name = "${each.key}-ghe-backup-user"
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 resource "aws_iam_access_key" "gh_backup_user_key" {
-  user = aws_iam_user.ghe_backup_user.name
+  for_each = toset(var.branch)
+
+  user = aws_iam_user.ghe_backup_user[each.key].name
 
   lifecycle {
     create_before_destroy = true
